e701ab2bef0b044b76c0c5b8fbaad4b7904c76f2
Zahin Mohammad <zahin.dev@gmail.com>
2019-11-17 18:20:27 -0800

    forgot to change file name

--- gh-actions-post-clone.py
+++ gh-actions-post-clone.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import sys
 
-app_center_key = sys.argv[1]
+app_center_key = sys.argv[1]    
 firebase_dev_json  = sys.argv[2]
 firebase_prod_json  = sys.argv[3]
 
-with open("./keys2.properties", "w") as text_file:
+with open("./keys.properties", "w") as text_file:
     text_file.write("HOCKEY_APP_SECRET=\"{}\"".format(app_center_key))
 
 # TODO: Prod and Dev flavors
