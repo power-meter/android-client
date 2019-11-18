6f01c28add8dad4a8eb449f15af2b0a3732d177a
Zahin Mohammad <zahin.dev@gmail.com>
2019-11-17 17:52:38 -0800

    convert to python

--- gh-actions-post-clone.py
+++ gh-actions-post-clone.py
@@ -1,13 +1,13 @@
-import os 
-h  = os.environ['hello_world']
-print(h)
-# firebase_prod_json  = os.environ['firebase_prod_json']
-# # firebase_dev_json  = os.environ['firebase_dev_json']
-# app_center_key = os.environ['app_center_secret']
+#!/usr/bin/env python3
+import sys
+
+firebase_prod_json  = sys.argv[1]
+firebase_dev_json  = sys.argv[2]
+app_center_key = sys.argv[3]
 
 # # TODO: Prod and Dev flavors
-# with open("./app/google-services.json", "w") as text_file:
-#     text_file.write(firebase_prod_json)
+with open("./app/google-services.json", "w") as text_file:
+    text_file.write(firebase_prod_json)
 
-# with open("./keys.properties", "w") as text_file:
-#     text_file.write(app_center_key)
+with open("./keys.properties", "w") as text_file:
+    text_file.write(app_center_key)
