2b69402ce4c26bf8dc4e7d1cf12a73eb838eb388
Zahin Mohammad <zahin.dev@gmail.com>
2019-11-17 21:11:24 -0800

    integrated jetpack navigation

--- app/src/main/java/io/mochahub/powermeter/MainActivity.kt
+++ app/src/main/java/io/mochahub/powermeter/MainActivity.kt
@@ -2,6 +2,9 @@ package io.mochahub.powermeter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.push.Push;
 import com.microsoft.appcenter.analytics.Analytics;
@@ -13,14 +16,18 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         AppCenter.start(application, BuildConfig.HOCKEY_APP_SECRET,
             Push::class.java,  Analytics::class.java, Crashes::class.java)
         Analytics.trackEvent("Hello World")
 
-        supportFragmentManager
-            .beginTransaction()
-            .add(R.id.fragmentLayout, MainFragment.newInstance(), "mainFragment")
-            .commit()
+        setContentView(R.layout.activity_main)
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        setupBottomNavMenu(navController)
+    }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        bottom_navigation?.let {
+            NavigationUI.setupWithNavController(it, navController)
+        }
     }
 }
