cc6de45a24a780a27d664dc03e9028bf57cee0b5
Zahin Mohammad <zahin.dev@gmail.com>
2019-11-17 18:18:33 -0800

    please....

--- gh-actions-post-clone.py
+++ gh-actions-post-clone.py
@@ -5,8 +5,8 @@ app_center_key = sys.argv[1]
 firebase_dev_json  = sys.argv[2]
 firebase_prod_json  = sys.argv[3]
 
-with open("./keys.properties", "w") as text_file:
-    text_file.write(app_center_key)
+with open("./keys2.properties", "w") as text_file:
+    text_file.write("HOCKEY_APP_SECRET=\"{}\"".format(app_center_key))
 
 # TODO: Prod and Dev flavors
 with open("./app/google-services.json", "w") as text_file:
