893db1965188fb39787661487571d9f17e7bd1c7
Zahin Mohammad <zahin.dev@gmail.com>
2019-11-17 16:33:42 -0800

    remove endlines

--- gh_actions_keys.sh
+++ gh_actions_keys.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 # Generate's key files from env variables
-echo "============================================================================== 
-Task         : Keys file 
-Description  : Create keys file from ENV variables for android gradle scripts 
-Version      : 1.0.0 
-Author       : Zahin 
+echo "==============================================================================
+Task         : Keys file
+Description  : Create keys file from ENV variables for android gradle scripts
+Version      : 1.0.0
+Author       : Zahin
 =============================================================================="
 echo "HOCKEY_APP_SECRET=$0" >./keys.properties
 
-echo "============================================================================== 
+echo "==============================================================================
 Task         : Firebase JSON
 Description  : Create Dev and Prod JSON files from ENV variables
-Version      : 1.0.0 
-Author       : Zahin 
+Version      : 1.0.0
+Author       : Zahin
 =============================================================================="
 echo "TODO: Update paths once dev and prod flavours are made"
 # echo $1|sed 's/\\//g'>./app/src/dev/google-services.json
@@ -21,4 +21,3 @@ echo "TODO: Update paths once dev and prod flavours are made"
 # cat ./app/src/google-services.json
 touch "./app/src/google-services.json"
 echo "$2" > "./app/src/google-services.json"
-
