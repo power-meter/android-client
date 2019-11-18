74145ef78b52165ac73825936700971b36bead9d
Zahin Mohammad <zahin.dev@gmail.com>
2019-11-17 16:17:11 -0800

    update script paths

--- gh_actions_keys.sh
+++ gh_actions_keys.sh
@@ -6,7 +6,7 @@ Description  : Create keys file from ENV variables for android gradle scripts
 Version      : 1.0.0 
 Author       : Zahin 
 =============================================================================="
-echo "HOCKEY_APP_SECRET=$0" >../keys.properties
+echo "HOCKEY_APP_SECRET=$0" >./keys.properties
 
 echo "============================================================================== 
 Task         : Firebase JSON
@@ -14,7 +14,7 @@ Description  : Create Dev and Prod JSON files from ENV variables
 Version      : 1.0.0 
 Author       : Zahin 
 =============================================================================="
-echo $1|sed 's/\\//g'>./src/dev/google-services.json
-cat ./src/dev/google-services.json
-echo $2|sed 's/\\//g'>./src/prod/google-services.json
+# echo $1|sed 's/\\//g'>./app/src/dev/google-services.json
+# cat ./src/dev/google-services.json
+echo $2|sed 's/\\//g'>./app/src/prod/google-services.json
 cat ./src/prod/google-services.json
