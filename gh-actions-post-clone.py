3d856d7081ab279fe09ac8688b6e5348efe5bd89
Zahin Mohammad <zahin.dev@gmail.com>
2019-11-17 18:11:19 -0800

    update argument order

--- gh-actions-post-clone.py
+++ gh-actions-post-clone.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 import sys
 
-firebase_prod_json  = sys.argv[1]
+app_center_key = sys.argv[1]
 firebase_dev_json  = sys.argv[2]
-app_center_key = sys.argv[3]
-
-# # TODO: Prod and Dev flavors
-with open("./app/google-services.json", "w") as text_file:
-    text_file.write(firebase_prod_json)
+firebase_prod_json  = sys.argv[3]
 
 with open("./keys.properties", "w") as text_file:
     text_file.write(app_center_key)
+
+# TODO: Prod and Dev flavors
+with open("./app/google-services.json", "w") as text_file:
+    text_file.write(firebase_prod_json)
+
