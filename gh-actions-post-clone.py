2c58db20858b876c271f7029250840f72076d148
Zahin Mohammad <zahin.dev@gmail.com>
2019-11-17 17:44:31 -0800

    test...

---
+++ gh-actions-post-clone.py
@@ -0,0 +1,13 @@
+import os 
+h  = os.environ['hello_world']
+print(h)
+# firebase_prod_json  = os.environ['firebase_prod_json']
+# # firebase_dev_json  = os.environ['firebase_dev_json']
+# app_center_key = os.environ['app_center_secret']
+
+# # TODO: Prod and Dev flavors
+# with open("./app/google-services.json", "w") as text_file:
+#     text_file.write(firebase_prod_json)
+
+# with open("./keys.properties", "w") as text_file:
+#     text_file.write(app_center_key)
--- gh_actions_keys.sh
+++ gh_actions_keys.sh
@@ -1,22 +1,23 @@
 #!/usr/bin/env bash
 # Generate's key files from env variables
-echo "Provided $# arguments"
-echo "==============================================================================
-Task         : Keys file
-Description  : Create keys file from ENV variables for android gradle scripts
-Version      : 1.0.0
-Author       : Zahin
-=============================================================================="
-echo "HOCKEY_APP_SECRET=$0" >./keys.properties
+echo "::set-env name=hello_world::$2"
+# echo "Provided $# arguments"
+# echo "==============================================================================
+# Task         : Keys file
+# Description  : Create keys file from ENV variables for android gradle scripts
+# Version      : 1.0.0
+# Author       : Zahin
+# =============================================================================="
+# echo "HOCKEY_APP_SECRET=$0" >./keys.properties
 
-echo "==============================================================================
-Task         : Firebase JSON
-Description  : Create Dev and Prod JSON files from ENV variables
-Version      : 1.0.0
-Author       : Zahin
-=============================================================================="
-echo "TODO: Update paths once dev and prod flavours are made"
-# echo $1|sed 's/\\//g'>./app/src/dev/google-services.json
-# cat ./src/dev/google-services.json
-echo "$2"|sed 's/\\//g'>./app/google-services.json
-cat ./app/google-services.json
+# echo "==============================================================================
+# Task         : Firebase JSON
+# Description  : Create Dev and Prod JSON files from ENV variables
+# Version      : 1.0.0
+# Author       : Zahin
+# =============================================================================="
+# echo "TODO: Update paths once dev and prod flavours are made"
+# # echo $1|sed 's/\\//g'>./app/src/dev/google-services.json
+# # cat ./src/dev/google-services.json
+# echo "$2"|sed 's/\\//g'>./app/google-services.json
+# cat ./app/google-services.json
