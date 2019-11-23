00b8ac72b6752aacee9b22eeaf627022d0ab47a6
Zahin Mohammad <zahin.mohammad@gmail.com>
2019-11-23 13:54:08 -0800

    clean + dark mode

--- app/src/main/java/io/mochahub/powermeter/stats/StatsFragment.kt
+++ app/src/main/java/io/mochahub/powermeter/stats/StatsFragment.kt
@@ -6,15 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.mochahub.powermeter.R
 import kotlinx.android.synthetic.main.fragment_stats.*
-
-
-
-
 
 class StatsFragment : Fragment() {
 
