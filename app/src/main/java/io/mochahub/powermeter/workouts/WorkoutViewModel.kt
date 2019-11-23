4b3661f3fde6e9318bbb4c4e34f5849af8f8d677
Atul Bipin <atulbipin@gmail.com>
2019-11-23 14:35:30 -0500

    Add workouts package and WorkoutViewModel

---
+++ app/src/main/java/io/mochahub/powermeter/workouts/WorkoutViewModel.kt
@@ -0,0 +1,29 @@
+package io.mochahub.powermeter.workouts
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.time.Instant
+import java.time.format.DateTimeFormatter
+
+class WorkoutViewModel : ViewModel() {
+    val _workouts = MutableLiveData<List<String>>(
+        listOf(
+            "Workout #1",
+            "Workout #2"
+        )
+    )
+
+    val workouts: LiveData<List<String>> = _workouts
+
+    fun addWorkout(
+        workoutName: String = DateTimeFormatter
+            .ofPattern("LLL dd yyyy (E) - HH:mm")
+            .format(Instant.now())
+    ) {
+        val currentList = workouts.value ?: listOf()
+        val newList = currentList.toMutableList()
+        newList.add(workoutName)
+        _workouts.postValue(newList)
+    }
+}
