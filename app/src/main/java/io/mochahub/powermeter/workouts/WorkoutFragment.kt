916191ff8c19762ad2ec978c1977888741b85eaf
Atul Bipin <atulbipin@gmail.com>
2019-11-24 15:54:20 -0500

    Working delete and undo

--- app/src/main/java/io/mochahub/powermeter/workouts/WorkoutFragment.kt
+++ app/src/main/java/io/mochahub/powermeter/workouts/WorkoutFragment.kt
@@ -1,5 +1,6 @@
 package io.mochahub.powermeter.workouts
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,7 +9,10 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import io.mochahub.powermeter.R
 import io.mochahub.powermeter.models.WorkoutSession
 import kotlinx.android.synthetic.main.fragment_workout.*
@@ -41,12 +45,42 @@ class WorkoutFragment : Fragment() {
 
         // TODO: Show a dialog fragment with editor where we can CRUD new session
         addSessionButton.setOnClickListener {
-            viewModel.addWorkout()
+            viewModel.addWorkoutSession()
         }
+
+        enableSwipeToDelete()
     }
 
     // TODO: Show a dialog fragment with editor where we can CRUD this session
     private fun onWorkoutSessionClicked(workoutSession: WorkoutSession) {
         Toast.makeText(requireContext(), "Clicked: ${workoutSession.date}", Toast.LENGTH_LONG).show()
+    }
+
+    private fun enableSwipeToDelete() {
+        val simpleItemTouchCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.adapterPosition
+
+                val deletedWorkoutSession = viewModel.removeWorkoutSession(position)
+                Snackbar.make(viewHolder.itemView, "Workout session deleted!", Snackbar.LENGTH_LONG).apply {
+                    setAction("UNDO") {
+                        viewModel.restoreWorkoutSession(position, deletedWorkoutSession)
+                    }
+                    setActionTextColor(Color.YELLOW)
+                    show()
+                }
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(simpleItemTouchCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
     }
 }
