bcc9ac3385d75fe7a04dd6426ffe42f2f2093d0f
Brian Norman <b2norman@uwaterloo.ca>
2019-11-24 15:38:56 -0800

    Skrr

--- app/src/main/java/io/mochahub/powermeter/exercises/EditExerciseFragment.kt
+++ app/src/main/java/io/mochahub/powermeter/exercises/EditExerciseFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.doOnTextChanged
 import androidx.fragment.app.DialogFragment
 import io.mochahub.powermeter.R
+import kotlinx.android.synthetic.main.fragment_edit_exercise.*
 
 class EditExerciseFragment : DialogFragment() {
     override fun onCreateView(
@@ -14,5 +16,15 @@ class EditExerciseFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_edit_exercise, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        exerciseNameText.doOnTextChanged { text, start, count, after ->
+            saveBtn.isEnabled = text!!.isNotEmpty()
+        }
+        saveBtn.setOnClickListener {
+            dismiss()
+        }
     }
 }
