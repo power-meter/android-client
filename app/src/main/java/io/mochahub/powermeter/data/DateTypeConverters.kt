276c92ca4093500b73fe2046f8540d267133e509
Brian Norman <b2norman@uwaterloo.ca>
2019-11-24 18:49:51 -0800

    Fix typeconverter

--- app/src/main/java/io/mochahub/powermeter/data/DateTypeConverters.kt
+++ app/src/main/java/io/mochahub/powermeter/data/DateTypeConverters.kt
@@ -1,13 +1,13 @@
 package io.mochahub.powermeter.data
 
-import androidx.room.TypeConverters
+import androidx.room.TypeConverter
 import java.time.OffsetDateTime
 import java.time.format.DateTimeFormatter
 
 object DateTypeConverters {
     private val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
 
-    @TypeConverters
+    @TypeConverter
     @JvmStatic
     fun toOffsetDateTime(value: String?): OffsetDateTime? {
         return value?.let {
@@ -15,7 +15,7 @@ object DateTypeConverters {
         }
     }
 
-    @TypeConverters
+    @TypeConverter
     @JvmStatic
     fun fromOffsetDateTime(date: OffsetDateTime?): String? {
         return date?.format(formatter)
