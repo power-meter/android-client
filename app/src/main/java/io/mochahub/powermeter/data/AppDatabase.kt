d641049322492ef32efc4d331e62bac7ff623c55
Zahin Mohammad <zahin.mohammad@gmail.com>
2019-11-24 17:10:30 -0800

    format

--- app/src/main/java/io/mochahub/powermeter/data/AppDatabase.kt
+++ app/src/main/java/io/mochahub/powermeter/data/AppDatabase.kt
@@ -13,8 +13,8 @@ abstract class AppDatabase : RoomDatabase() {
         @Volatile private var instance: AppDatabase? = null
         private val LOCK = Any()
 
-        operator fun invoke(context: Context)= instance ?: synchronized(LOCK){
-            instance ?: buildDatabase(context).also { instance = it}
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also { instance = it }
         }
 
         private fun buildDatabase(context: Context) = Room.databaseBuilder(context,
