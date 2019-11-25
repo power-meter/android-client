25dbbbf0f57813e1e75d4214574f63ca84f9d0c9
Brian Norman <b2norman@uwaterloo.ca>
2019-11-24 17:21:44 -0800

    Launch in coroutiune

--- app/src/main/java/io/mochahub/powermeter/exercises/ExerciseViewModel.kt
+++ app/src/main/java/io/mochahub/powermeter/exercises/ExerciseViewModel.kt
@@ -4,11 +4,15 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import io.mochahub.powermeter.data.AppDatabase
 import io.mochahub.powermeter.data.Exercise
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class ExerciseViewModel(val db: AppDatabase) : ViewModel() {
     val exercises: LiveData<List<Exercise>> = db.exerciseDao().getAll()
 
     fun addExercise(exercise: Exercise) {
-        db.exerciseDao().insertAll(exercise)
+        GlobalScope.launch {
+            db.exerciseDao().insertAll(exercise)
+        }
     }
 }
