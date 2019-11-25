59e098899ab263050f3a7774a7c7b45934897e62
Brian Norman <briankn8@gmail.com>
2019-11-24 19:01:04 -0800

    Delete exercises (#20)

--- app/src/main/java/io/mochahub/powermeter/data/ExerciseDao.kt
+++ app/src/main/java/io/mochahub/powermeter/data/ExerciseDao.kt
@@ -2,9 +2,9 @@ package io.mochahub.powermeter.data
 
 import androidx.lifecycle.LiveData
 import androidx.room.Dao
-import androidx.room.Query
-import androidx.room.Insert
 import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
 import androidx.room.Update
 
 @Dao
