a626517ded33fc3b9857ee6bafa1c5a242cc748b
Zahin Mohammad <zahin.mohammad@gmail.com>
2019-11-26 01:22:08 -0800

    working menu with switching theme

--- app/src/main/java/io/mochahub/powermeter/MainActivity.kt
+++ app/src/main/java/io/mochahub/powermeter/MainActivity.kt
@@ -1,7 +1,12 @@
 package io.mochahub.powermeter
 
+import android.content.SharedPreferences
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
@@ -12,6 +17,9 @@ import com.microsoft.appcenter.push.Push
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val preferences: SharedPreferences
+        get() = PreferenceManager.getDefaultSharedPreferences(this)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,5 +32,31 @@ class MainActivity : AppCompatActivity() {
 
         val navController: NavController = Navigation.findNavController(this, R.id.nav_host_fragment)
         NavigationUI.setupWithNavController(bottom_navigation, navController)
+
+        // debug menu listener
+        val listener = SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
+            if (sharedPreferences.getBoolean(key, false)) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+        preferences.registerOnSharedPreferenceChangeListener(listener)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        return when (item?.itemId) {
+            R.id.action_settings -> {
+                Navigation.findNavController(this, R.id.nav_host_fragment).navigate(R.id.settingsFragment)
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
