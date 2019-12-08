e5353a0ec01c71aeafdf461034bc8cc7f3c44904
Zahin Mohammad <zahin.mohammad@gmail.com>
2019-12-07 21:58:24 -0800

    navigate to add workout fragment

--- app/src/main/java/io/mochahub/powermeter/workouts/WorkoutFragment.kt
+++ app/src/main/java/io/mochahub/powermeter/workouts/WorkoutFragment.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -50,7 +51,8 @@ class WorkoutFragment : Fragment() {
 
         // TODO: Show a dialog fragment with editor where we can CRUD new session
         addSessionButton.setOnClickListener {
-            viewModel.addWorkoutSession()
+            this.findNavController().navigate(R.id.action_destination_workout_session_screen_to_addWorkout)
+//            viewModel.addWorkoutSession()
         }
 
         enableSwipeToDelete()
