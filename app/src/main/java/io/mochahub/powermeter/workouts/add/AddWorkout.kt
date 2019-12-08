e9b588a0cbff4129c1139936471b8657235c53e3
Zahin Mohammad <zahin.mohammad@gmail.com>
2019-12-07 21:31:31 -0800

    init

---
+++ app/src/main/java/io/mochahub/powermeter/workouts/add/AddWorkout.kt
@@ -0,0 +1,32 @@
+package io.mochahub.powermeter.workouts.add
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import io.mochahub.powermeter.R
+
+class AddWorkout : Fragment() {
+
+    companion object {
+        fun newInstance() = AddWorkout()
+    }
+
+    private lateinit var viewModel: AddWorkoutViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.add_workout_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(AddWorkoutViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+}
---
+++ app/src/main/java/io/mochahub/powermeter/workouts/add/AddWorkoutViewModel.kt
@@ -0,0 +1,7 @@
+package io.mochahub.powermeter.workouts.add
+
+import androidx.lifecycle.ViewModel
+
+class AddWorkoutViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
