bf200d5b28d4f8b89d84355b7117f637d9f4d4c2
Brian Norman <briankn8@gmail.com>
2019-12-12 22:16:02 -0800

    [PM-13] Add new exercises (#29)
    * Add new exercises
    
    * Clear new exercise in shared viewmodel
    
    * Change icon
    
    * String resources
    
    * Update app/src/main/res/layout/dialog_new_exercise.xml
    
    * Update app/src/main/res/layout/dialog_new_exercise.xml
    
    * Update app/src/main/res/layout/dialog_new_exercise.xml
    
    * Update app/src/main/res/layout/dialog_new_exercise.xml

---
+++ app/src/main/java/io/mochahub/powermeter/exercises/NewExerciseSharedViewModel.kt
@@ -0,0 +1,19 @@
+package io.mochahub.powermeter.exercises
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.mochahub.powermeter.models.Exercise
+
+class NewExerciseSharedViewModel : ViewModel() {
+    private val _newExercise = MutableLiveData<Exercise>()
+    val newExercise: LiveData<Exercise> = _newExercise
+
+    fun saveNewExercise(exercise: Exercise) {
+        _newExercise.postValue(exercise)
+    }
+
+    fun clearNewExercise() {
+        _newExercise.postValue(null)
+    }
+}
