2d521f69b3d9fe9733c1568b08d57dfc53b141c6
Zahin Mohammad <zahin.mohammad@gmail.com>
2019-12-14 14:12:14 -0800

    date picker

--- app/src/main/java/io/mochahub/powermeter/workoutsession/NewWorkoutDialog.kt
+++ app/src/main/java/io/mochahub/powermeter/workoutsession/NewWorkoutDialog.kt
@@ -1,11 +1,16 @@
 package io.mochahub.powermeter.workoutsession
 
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import io.mochahub.powermeter.R
+import kotlinx.android.synthetic.main.dialog_new_workout_dialog.newWorkoutDateText
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class NewWorkoutDialog : DialogFragment() {
 
@@ -32,8 +37,33 @@ class NewWorkoutDialog : DialogFragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.dialog_new_workout_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initDateField()
+    }
+
+    private fun initDateField() {
+        val myCalendar = Calendar.getInstance()
+        val dateSetListener = DatePickerDialog.OnDateSetListener {
+                datePicker, year, month, day -> myCalendar.apply {
+            set(Calendar.YEAR, year)
+            set(Calendar.MONTH, month)
+            set(Calendar.DAY_OF_MONTH, day)
+            // TODO: Make a const
+            val myFormat = "mm/dd/yy"
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            newWorkoutDateText.setText(sdf.format(myCalendar.time))
+        }
+        }
+
+        newWorkoutDateText.setOnClickListener {
+            DatePickerDialog(requireContext(), dateSetListener, myCalendar
+                .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                myCalendar.get(Calendar.DAY_OF_MONTH)).show()
+        }
     }
 }
