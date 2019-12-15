97c349d3f0fd3a51de42f4d28564957ab4c4a427
Brian Norman <b2norman@uwaterloo.ca>
2019-12-15 08:13:11 -0800

    Remove bulk

--- app/src/main/java/io/mochahub/powermeter/models/StatCard.kt
+++ app/src/main/java/io/mochahub/powermeter/models/StatCard.kt
@@ -3,10 +3,4 @@ package io.mochahub.powermeter.models
 data class StatCard(
     var title: String,
     var exercise: Exercise
-
-) {
-
-    override fun toString(): String {
-        return "StatCard(title='$title', body='$exercise')"
-    }
-}
+)
