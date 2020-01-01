b70f0773e2d20db3182aa8fb894c2c2ee9ba707c
Brian Norman <b2norman@uwaterloo.ca>
2020-01-01 11:38:28 -0700

    Prefer extension functions to member functions

--- app/src/main/java/io/mochahub/powermeter/models/Workout.kt
+++ app/src/main/java/io/mochahub/powermeter/models/Workout.kt
@@ -3,24 +3,16 @@ package io.mochahub.powermeter.models
 data class Workout(
     val exercise: Exercise,
     val sets: List<WorkoutSet>
-) {
-    fun addSet(set: WorkoutSet): Workout {
-        return this.copy(sets = sets + listOf(set))
-    }
+)
 
-    fun removeSet(index: Int): Workout {
-        val setsList = sets.toMutableList().apply {
-            this.removeAt(index)
-        }
+fun Workout.addSet(set: WorkoutSet): Workout = this.copy(sets = sets + listOf(set))
 
-        return this.copy(sets = setsList)
-    }
+fun Workout.removeSet(index: Int): Workout {
+    val setsList = sets.toMutableList().apply { this.removeAt(index) }
+    return this.copy(sets = setsList)
+}
 
-    fun updateSet(index: Int, set: WorkoutSet): Workout {
-        val setsList = sets.toMutableList().apply {
-            this[index] = set
-        }
-
-        return this.copy(sets = setsList)
-    }
+fun Workout.updateSet(index: Int, set: WorkoutSet): Workout {
+    val setsList = sets.toMutableList().apply { this[index] = set }
+    return this.copy(sets = setsList)
 }
