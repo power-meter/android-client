0c2dc0253d47eb4d9708aa108cf57d69604f2b35
Brian Norman <b2norman@uwaterloo.ca>
2019-12-31 17:18:20 -0700

    Move Exercise CRUD over to IO thread (#42)

--- app/src/main/java/io/mochahub/powermeter/data/AppDatabase.kt
+++ app/src/main/java/io/mochahub/powermeter/data/AppDatabase.kt
@@ -6,21 +6,24 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [ExerciseEntity::class], version = 1)
+@Database(entities = [ExerciseEntity::class], version = 1, exportSchema = false)
 @TypeConverters(DateTypeConverters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun exerciseDao(): ExerciseDao
 
     companion object {
-        @Volatile private var instance: AppDatabase? = null
+        @Volatile
+        private var instance: AppDatabase? = null
         private val LOCK = Any()
 
         operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
             instance ?: buildDatabase(context).also { instance = it }
         }
 
-        private fun buildDatabase(context: Context) = Room.databaseBuilder(context,
-            AppDatabase::class.java, "power-meter.db")
+        private fun buildDatabase(context: Context) = Room.databaseBuilder(
+            context,
+            AppDatabase::class.java, "power-meter.db"
+        )
             .build()
     }
 }
