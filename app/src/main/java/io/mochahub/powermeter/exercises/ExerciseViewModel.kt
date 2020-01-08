5c6431a40422e0fb0bcfd0f6e80c88d88767f40c
Brian Norman <b2norman@uwaterloo.ca>
2020-01-08 12:39:37 -0700

    Use Flow for exercises (#48)

--- app/src/main/java/io/mochahub/powermeter/exercises/ExerciseViewModel.kt
+++ app/src/main/java/io/mochahub/powermeter/exercises/ExerciseViewModel.kt
@@ -2,32 +2,26 @@ package io.mochahub.powermeter.exercises
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import io.mochahub.powermeter.data.AppDatabase
 import io.mochahub.powermeter.data.ExerciseEntity
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class ExerciseViewModel(val db: AppDatabase) : ViewModel() {
-    val exercises: LiveData<List<ExerciseEntity>> = db.exerciseDao().getAll()
+    val exercises: LiveData<List<ExerciseEntity>> = db.exerciseDao().getAll().asLiveData()
 
     fun addExercise(exercise: ExerciseEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
-            db.exerciseDao().insertAll(exercise)
-        }
+        viewModelScope.launch { db.exerciseDao().insertAll(exercise) }
     }
 
     fun removeExercise(position: Int): ExerciseEntity {
         val exercise = exercises.value!![position]
-        viewModelScope.launch(Dispatchers.IO) {
-            db.exerciseDao().delete(exercise)
-        }
+        viewModelScope.launch { db.exerciseDao().delete(exercise) }
         return exercise
     }
 
     fun updateExercise(exercise: ExerciseEntity) {
-        viewModelScope.launch(Dispatchers.IO) {
-            db.exerciseDao().updateExercise(exercise)
-        }
+        viewModelScope.launch { db.exerciseDao().updateExercise(exercise) }
     }
 }
